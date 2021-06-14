37ae17d4da85275c9d33a98ae566c124b9690295
진이지 <w2018@e-mirim.hs.kr>
2021-06-14 10:14:09 +0900

    01 음양더하기 반복문

--- 프로그래머스Lv1/01음양더하기.py
+++ 프로그래머스Lv1/01음양더하기.py
@@ -1,9 +1,23 @@
 def solution(absolutes, signs):
-    answer = 123456789
+    answer = 0
+    if signs[0] == True:
+        answer += absolutes[0]
+    else:
+        answer -= absolutes[0]
+
+    if signs[1] == True:
+        answer += absolutes[1]
+    else:
+        answer -= absolutes[1]
+
+    if signs[2] == True:
+        answer += absolutes[2]
+    else:
+        answer -= absolutes[2]
     return answer
 
 
 if __name__ == "__main__":
     abs = [4, 7, 12]
     s = [True, False, True]
-    solution(abs, s)
+    print(solution(abs, s))

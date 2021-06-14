2dcae5adde4559dc543cf739eba2c790a3378dd0
진이지 <w2018@e-mirim.hs.kr>
2021-06-14 10:10:24 +0900

    음양더하기

--- 연습.py
+++ 연습.py
@@ -2,3 +2,6 @@ str1 = "ABCDEFG"
 
 if "ABC" in str1:
     print("참")
+
+a = [1,2,3,4]
+b=[-3,-1,0,2]
---
+++ 프로그래머스Lv1/01음양더하기.py
@@ -0,0 +1,9 @@
+def solution(absolutes, signs):
+    answer = 123456789
+    return answer
+
+
+if __name__ == "__main__":
+    abs = [4, 7, 12]
+    s = [True, False, True]
+    solution(abs, s)

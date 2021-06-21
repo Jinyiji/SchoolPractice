9b592a9aced3862489d042e83d8278fa13690ab9
진이지 <w2018@e-mirim.hs.kr>
2021-06-21 10:11:14 +0900

    03 두 개 뽑아서 더하기(결과)

--- 프로그래머스Lv1/03두 개 뽑아서 더하기.py
+++ 프로그래머스Lv1/03두 개 뽑아서 더하기.py
@@ -11,17 +11,17 @@ def solution(numbers):
     for i in range(len(numbers)-1):
         for j in range(i+1, len(numbers)):
             value = numbers[i] + numbers[j]
-            # 두 수를 뽑아 더한 값이 기존에 존재하지 않을 때
-            if value not in answer:
-                # list에 추가
-                answer.append(value)
-    # 오름차순으로 정렬
+            answer.append(value)
+    # 중복제거
+    answer = list(set(answer))
+    # 오름차순 정렬
     answer.sort()
     return answer
 
+
 def main():
     numbers = [5, 0, 2, 7]
-    print(solution(numbers))
+    print( solution(numbers) )
 
 
 if __name__ == "__main__":

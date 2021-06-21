7933e5b4cae53ac0b4990cd018a8b7dd29bec441
진이지 <w2018@e-mirim.hs.kr>
2021-06-21 09:16:16 +0900

    03 두 개 뽑아서 더하기(시작)

---
+++ 프로그래머스Lv1/03두 개 뽑아서 더하기.py
@@ -0,0 +1,17 @@
+# 정수 배열 numbers가 주어집니다.
+# numbers에서 서로 다른 인덱스에 있는
+# 두 개의 수를 뽑아 더해서 만들 수 있는 모든 수를
+# 배열에 오름차순으로 담아 return 하도록
+# solution 함수를 완성해주세요.
+
+def solution(numbers):
+    answer = []
+    return answer
+
+def main():
+    numbers = [5, 0, 2, 7]
+    print(solution(numbers))
+
+
+if __name__ == "__main__":
+    main()
